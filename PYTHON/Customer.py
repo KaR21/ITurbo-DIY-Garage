f3e2e6b19d4f788bf32786d1e8f2b37217dc4d2f
Karmele Elorza <96118700+KaR21@users.noreply.github.com>
2022-05-06 10:08:35 +0200

    Delete Customer.py

--- PYTHON/Customer.py
+++
@@ -1,9 +0,0 @@
-from Person import *
-import Basic_Metods
-
-class Customer(Person):
-    def __init__(self, id, name, surname, phone_home, phone_mobile, email, address, city, postcode, condition):
-        super().__init__(id, name, surname, phone_home, phone_mobile, email, address, city, postcode, condition)
-
-
-
